7cb5fc5775212ba67cd110fbdc384f8752ffc3c3
Andreea <andreeapopovici99929@gmail.com>
2021-05-22 15:28:19 +0300

    AnimalController

---
+++ src/main/java/com/p5/adoptions/controllers/AnimalController.java
@@ -0,0 +1,20 @@
+package com.p5.adoptions.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/animals")
+public class AnimalController
+{
+    //OLD WAY: No Bueno: @RequestMapping(method = RequestMethod.GET,value = "/hello")
+    @GetMapping("/hello")
+    public ResponseEntity<String> greetOwner(@RequestParam(name="name", required = false)String ownerName)
+    {
+        String name=ownerName==null?"world": ownerName;
+        return ResponseEntity.ok("Hello" + name + ",from the animal shelter!");
+    }
+}
